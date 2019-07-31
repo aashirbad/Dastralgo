38a72ddb500d971618217df026407c92918fb89a
satya pati <iamsatyapati@gmail.com>
2019-07-31 17:47:11 +0530

    Queue updated

---
+++ datastructures/queue/queue.py
@@ -0,0 +1,21 @@
+from singlylinkedlist import *
+
+class Stack:
+	def __init__(self):
+		self.LinkedList = LinkedList()
+
+
+	def push(self,key):
+		self.LinkedList.pushfront(key)
+
+	def pop(self):
+		self.LinkedList.popback()
+
+	def top(self):
+		self.LinkedList.topback()
+
+	def isempty(self):
+		return self.LinkedList.isempty()
+
+	def printData(self):
+		self.LinkedList.printData()
